75da88aa4102bac22e9904bfe60ba2a5caef8277
Lauren Hutchinson <Lauren Hutchinson>
2017-06-28 20:44:27 -0500

    starting

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<OXJaxvnkLgxWEktQfG0As8CJw>',
+  consumer_secret: '<LwuBzMqqBkEh0m49AezHjDxvIvGbEhO5PJiWPnNSVm1AY1cFFF>',
+  access_token_key: '<	879831270842404864-f8AwIiJnGiJYVAFD9pa1FmRipZWoIOs>',
+  access_token_secret: '<bl6zO0cEcZvAZEM8F3Sqbbptrc45X5G71uEkA4SkXQvOz>',
+}
---
+++ liri.js
@@ -0,0 +1,10 @@
+//code you need to grab the data from keys.js. 
+//Then store the keys in a variable.
+//Make it so liri.js can take in one of the following commands:
+
+
+
+// my-tweets
+// spotify-this-song
+// movie-this
+// do-what-it-says
