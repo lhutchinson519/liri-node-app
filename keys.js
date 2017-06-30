39c13b5c4329953772e4a24ad719ab456b1c2f7f
Lauren Hutchinson <Lauren Hutchinson>
2017-06-30 17:16:12 -0500

    tweets working

--- keys.js
+++ keys.js
@@ -1,8 +1,13 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<OXJaxvnkLgxWEktQfG0As8CJw>',
-  consumer_secret: '<LwuBzMqqBkEh0m49AezHjDxvIvGbEhO5PJiWPnNSVm1AY1cFFF>',
-  access_token_key: '<	879831270842404864-f8AwIiJnGiJYVAFD9pa1FmRipZWoIOs>',
-  access_token_secret: '<bl6zO0cEcZvAZEM8F3Sqbbptrc45X5G71uEkA4SkXQvOz>',
+  consumer_key: 'OXJaxvnkLgxWEktQfG0As8CJw',
+  consumer_secret: 'LwuBzMqqBkEh0m49AezHjDxvIvGbEhO5PJiWPnNSVm1AY1cFFF',
+  access_token_key: '879831270842404864-f8AwIiJnGiJYVAFD9pa1FmRipZWoIOs',
+  access_token_secret: 'bl6zO0cEcZvAZEM8F3Sqbbptrc45X5G71uEkA4SkXQvOz',
 }
+
+// exports.spotifyKeys = {
+// 	client_id = 'c61b6feb748443afb6987bde99e3b7ee'; // Your client id
+// 	client_secret = '5bd6a15e62524ea5a1158726dda636ee'; // Your secret
+// }
--- liri.js
+++ liri.js
@@ -1,10 +1,62 @@
 //code you need to grab the data from keys.js. 
 //Then store the keys in a variable.
-//Make it so liri.js can take in one of the following commands:
+var request = require("request");
+//fs package to read and write
+var fs = require("fs");
+var stuffINeed = require("./keys.js");
+var action = process.argv[2];
 
+var Twitter = require('twitter');
 
+var client = new Twitter({
+    consumer_key: 'OXJaxvnkLgxWEktQfG0As8CJw',
+    consumer_secret: 'LwuBzMqqBkEh0m49AezHjDxvIvGbEhO5PJiWPnNSVm1AY1cFFF',
+    access_token_key: '879831270842404864-f8AwIiJnGiJYVAFD9pa1FmRipZWoIOs',
+    access_token_secret: 'bl6zO0cEcZvAZEM8F3Sqbbptrc45X5G71uEkA4SkXQvOz'
+});
 
+var params = { lhutch519: 'nodejs' };
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+        console.log(tweets);
+
+        for (i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].text);
+        };
+    };
+});
+
+//COMMANDS
 // my-tweets
-// spotify-this-song
+console.log(stuffINeed)
+
+switch (action) {
+
+    // case "my-tweets":
+    //     myTweets();
+    //     break;
+
+    case "spotify-this-song":
+        spotifySong();
+        break;
+}
+
+// function myTweets() {
+//     fs.readFile("")
+// }
+
+
+function spotifySong() {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log(data);
+    });
+}
+
 // movie-this
+
 // do-what-it-says
